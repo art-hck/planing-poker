068be15cc5f4059c991ee9ac165036d7f2a7b364
a.baydin <a.baydin@etpgpb.ru>
2022-02-16 00:47:19 +0000

    lazy loading preloading strategy

--- planing-poker/src/app/app-routing.module.ts
+++ planing-poker/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from "./components/not-found/not-found.component";
 import { RoomsComponent } from "./components/rooms/rooms.component";
 import { AuthGuard } from "./components/auth/auth.guard";
@@ -12,7 +12,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
